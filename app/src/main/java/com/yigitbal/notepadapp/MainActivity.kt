94af8ff7af96a3d037a048a4bbd0662563e2dea2
Yigit Balbasi <yigitbalbasi@gmail.com>
2023-07-08 13:47:41 +0300

    'uygulama_gelistiriliyor'

---
+++ app/src/main/java/com/yigitbal/notepadapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.yigitbal.notepadapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.yigitbal.notepadapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+}
